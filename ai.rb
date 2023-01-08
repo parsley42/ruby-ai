1c8b68c0569789aaeb789ec05d64f72ca38e2e21
David Parsley <parsley@linuxjedi.org>
2023-01-08 17:33:49 -0500

    Start on class, add examples

--- ai.rb
+++ ai.rb
@@ -2,12 +2,60 @@
 
 require "ruby/openai"
 
-Ruby::OpenAI.configure do |config|
-    config.access_token = ENV.fetch('OPENAI_KEY')
-    # config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
-end
+class AIChat
+    Preamble = <<-'EOF'
+A conversation between a Human software engineer and an AI mentor who provides sage advice and instructive explanations, often with formatted code samples showing example input and output, and with explanations of how the code works.
 
-client = OpenAI::Client.new
+Human: Who are you?
+AI: I am a wise and experienced engineer here to help you be a better engineer and person. How can I help you?
+EOF
+
+    def initialize
+        @model = "text-davinci-003"
+        @temperature = 0.84      # creativity
+        @max_tokens = 2212
+        @responses = 1           # n
+        @word_probability = 0.7  # top_p
+        @frequency_penalty = 0.2
+        @presence_penalty = 0.4
+        @num_beams = 7           # diversity of responses, good value per the AI
+        @stop = [" Human:", "AI:"]
+        Ruby::OpenAI.configure do |config|
+            config.access_token = ENV.fetch('OPENAI_KEY')
+            # config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
+        end
+        @client = OpenAI::Client.new
+        @exchanges = []
+    end
+
+    def query(input)
+        prompt = Preamble
+        @exchanges.each do |exchange|
+            prompt += "Human: #{exchange["human"]}\nAI: #{exchange["ai"]}\n"
+        end
+        prompt += "Human: #{input}\nAI: "
+        puts("******* Full prompt:\n#{prompt}\n")
+        response = @client.completions(parameters: {
+            model: @model,
+            prompt: prompt,
+            temperature: @temperature,
+            max_tokens: @max_tokens,
+            n: @responses,
+            top_p: @word_probability,
+            frequency_penalty: @frequency_penalty,
+            presence_penalty: @presence_penalty,
+            # num_beams: @num_beams,
+            stop: @stop
+        })
+        pp(response)
+        @exchanges << {
+            "human" => input,
+            "ai" => response["choices"][0]["text"]
+        }
+        pp(@exchanges)
+        return response["choices"][0]["text"]
+    end
+end
 
 def chat(client, prompt)
     response = client.completions(
@@ -25,10 +73,22 @@ def chat(client, prompt)
     return response["choices"][0]["text"]
 end
 
+chat = AIChat.new
+
+response = chat.query("What is the meaning of life?")
+puts(response)
+exit
+
+print "Query: "
+user_input = gets.chomp
+
+chat.query(user_input)
+exit
+
 current = ""
 
 while true
-    print "You: "
+    print "Query: "
     user_input = gets.chomp
     exit if user_input == "quit"
     current += user_input + "\n"
