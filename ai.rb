8852f9ed4a231a296cbb67c92bbaf49223f2d5c5
David Parsley <parsley@linuxjedi.org>
2023-01-08 13:04:34 -0500

    Improvements from the playground

--- ai.rb
+++ ai.rb
@@ -10,27 +10,30 @@ end
 client = OpenAI::Client.new
 
 def chat(client, prompt)
-  response = client.completions(
-    parameters: {
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 1024,
-        n: 1,
-        temperature: 0.5,
-    }
-  )
-
-  puts response["choices"].map { |c| c["text"] }
+    response = client.completions(
+        parameters: {
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 2048,
+            n: 1,
+            top_p: 1,
+            frequency_penalty: 1,
+            presence_penalty: 1,
+            temperature: 0.7,
+        }
+    )
+    return response["choices"][0]["text"]
 end
 
-prompt = ""
+current = ""
 
 while true
-  print "You: "
-  user_input = gets.chomp
-#   prompt += " " + user_input
-    prompt = user_input
-  response = chat(client, prompt)
-  puts "Bot: #{response}"
-  #prompt += " " + response
+    print "You: "
+    user_input = gets.chomp
+    exit if user_input == "quit"
+    current += user_input + "\n"
+    puts("************** Full prompt:\n#{current}\n**************\n")
+    response = chat(client, current)
+    puts "Bot: #{response}"
+    current += response + "\n\n"
 end
